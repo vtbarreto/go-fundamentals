1e4efc6350a1763fe2c716ed4b6b480587da240f
vteixeira212 <vinicius.barreto@engie.com>
2021-04-19 22:50:44 -0300

    Slice copy

---
+++ arraysSlideMap/appendcopy/appendcopy.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	array1 := [3]int{1, 2, 3}
+	var slice1 []iny
+
+	slice1 = append(slice1, 4, 5, 6)
+	fmt.Println(array1, slice1)
+
+	slice2 := make([]int, 2)
+	copy(slice2, slice1)
+	fmt.Println(slice2)
+}
