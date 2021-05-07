5a7464175b65c4505a031a47b40f50517655d850
vteixeira212 <vteixeira212@gmail.com>
2021-05-06 23:00:48 -0300

    Closure

--- funcoes/variaticas 2/variaticas.go
+++ funcoes/variaticas 2/variaticas.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func candidatos(aprovados ...string) string {
+func candidatos(aprovados ...string) {
 	fmt.Println("Lista de Aprovados")
 	for i, aprovado := range aprovados {
 		fmt.Printf("%d0 %s\n", i+1, aprovado)
