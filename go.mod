ff0b0278911c450d0ce782c9454ccbf51ccebb44
Vinicius <vteixeira212@gmail.com>
2022-04-29 13:15:55 -0300

    usando pacote em outro projeto

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Fundamentos-em-Go
+
+go 1.18
+
+require github.com/ViniciusTeixeiraBarreto/area v0.0.0-20220429160726-044303e68de8
---
+++ pacote/reuso/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/ViniciusTeixeiraBarreto/area"
+)
+
+func main() {
+	fmt.Println(area.Circ(6.0))
+}
