c2c7039016338b192ac7919bf61e11992d148806
Vinicius <vteixeira212@gmail.com>
2022-04-29 12:44:17 -0300

    Multiplos arquivos

---
+++ pacote/reta/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	p1 := Ponto{2.0, 2.0}
+	p2 := Ponto{2.0, 4.0}
+
+	fmt.Println(catetos(p1, p2))
+}
---
+++ pacote/reta/reta.go
@@ -0,0 +1,21 @@
+package main
+
+import "math"
+
+//Ponto representa uma coordenada no plano cartesiano
+type Ponto struct {
+	x float64
+	y float64
+}
+
+func catetos(a, b Ponto) (cx, cy float64) {
+	cx = math.Abs(b.x - a.x)
+	cy = math.Abs(b.y - a.y)
+	return
+}
+
+//Distancia é reponsavel por calcular a distancia linear entre dois pontos
+func Distancia(a, b Ponto) float64 {
+	cx, cy := catetos(a, b)
+	return math.Sqrt(math.Pow(cx, 2) + math.Pow(cy, 2))
+}
