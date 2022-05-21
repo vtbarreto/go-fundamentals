aea75f9e1a045b2df75d5a18e963406efaca5aed
Vinicius <vteixeira212@gmail.com>
2022-05-21 01:49:41 -0300

    utilizando dependencia propria

--- concorrencia/generator/generator.go
+++ concorrencia/generator/generator.go
@@ -2,33 +2,17 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"net/http"
-	"regexp"
+
+	gohtml "github.com/viniciusteixeirabarreto/gohtml"
 )
 
-// <- chan - canal somente-leitura
-func titulo(urls ...string) <-chan string {
-	c := make(chan string)
-	for _, url := range urls {
-		go func(url string) {
-			resp, _ := http.Get(url)
-			html, _ := ioutil.ReadAll(resp.Body)
-
-			r, _ := regexp.Compile("<title>(.*?)<\\/title>")
-			c <- r.FindStringSubmatch(string(html))[1]
-		}(url)
-	}
-	return c
-}
-
 func main() {
-	t1 := titulo("https://www.cod3r.com.br", "https://www.google.com")
-	t2 := titulo("https://www.amazon.com.br", "https://www.youtube.com")
+	t1 := gohtml.Titulo("https://www.cod3r.com.br", "https://www.google.com")
+	t2 := gohtml.Titulo("https://www.amazon.com.br", "https://www.youtube.com")
 
 	fmt.Println(<-t1, "|")
-	fmt.Print(<-t2, "|")
+	fmt.Println(<-t2, "|")
 
 	fmt.Println(<-t1, "|")
-	fmt.Print(<-t2, "|")
+	fmt.Println(<-t2, "|")
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/Fundamentos-em-Go
 
 go 1.18
 
-require github.com/ViniciusTeixeiraBarreto/area v0.0.0-20220429160726-044303e68de8
+require (
+	github.com/ViniciusTeixeiraBarreto/area v0.0.0-20220429160726-044303e68de8
+	github.com/viniciusteixeirabarreto/gohtml v1.0.0
+)
