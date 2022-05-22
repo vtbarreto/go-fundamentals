b65d71cdc92794846fa9addd687414da0c821842
Vinicius <vteixeira212@gmail.com>
2022-05-22 00:38:50 -0300

    servidor http com pagina dinamica

--- README.md
+++ README.md
@@ -92,7 +92,9 @@ Repositorio seguinto curso da Udemy <a href="https://www.udemy.com/share/101tNS3
 - [x] Transaction
 - [x] Select
 
+
 ##
 - [x] Servidor http com pagina static
+- [x] Servidor http com pagina dinamica
 
 <img src="https://simpleicons.org/icons/go.svg" width="50"/>
---
+++ http/dinamico/dinamico.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+func horaCerta(w http.ResponseWriter, r *http.Request) {
+	s := time.Now().Format("02/01/2006 03:04:05")
+	fmt.Fprintf(w, "<h1>Hora certa: %s</h1>", s)
+
+}
+
+func main() {
+	http.HandleFunc("/horacerta", horaCerta)
+	log.Println("exec")
+	log.Fatal(http.ListenAndServe(":3000", nil))
+}
