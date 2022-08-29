18c22d97de2d087430dd0b35671776415a6405d7
Vinicius <vteixeira212@gmail.com>
2022-08-28 23:18:04 -0300

    Funcao generica, aceitando dois tipos de variaveis, int e float

---
+++ funcoes/generico/generico.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func Sum[V int64 | float64](m []V) V {
+	var s V
+	for _, v := range m {
+		s += v
+	}
+	return s
+}
+
+func main() {
+	ints := []int64{1, 2, 3, 4, 5, 6}
+	floats := []float64{12.3, 13.5, 10.7, 11.42}
+	fmt.Println("soma int64:", Sum[int64](ints))
+	fmt.Println("soma float64:", Sum[float64](floats))
+}
